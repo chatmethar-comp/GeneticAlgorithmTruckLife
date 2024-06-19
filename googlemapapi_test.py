7673d959f14d9b2a272f00c42096e4aa5f9fd86c
ProjecTz20 <projectzazath1117@gmail.com>
2024-06-19 16:05:37 +0700

    add file

--- googlemapapi_test.py
+++ googlemapapi_test.py
@@ -1,38 +1,52 @@
 # pip install googlemaps
 import googlemaps
 from datetime import datetime
+import csv
 
-API_KEY = 'APIKEY'
+a = [[(0,0),(5,20),(6,20)],
+     [(5,20),(0,0),(4,20)],
+     [(6,20),(4,20),(0,0)]]
+
+API_KEY = 'AIzaSyC4SRkYCBiobk42kWn-WhhqEYAYUv8_WeI'
 gmaps = googlemaps.Client(key=API_KEY)
+start_coords = (13.743833, 100.562694)
 
-def get_travel_time(start_coords, dest_coords):
-    # Get directions
+def get_travel_time_and_distance(start_coords,dest_coords):
     directions_result = gmaps.directions(start_coords,
                                          dest_coords,
                                          mode="driving",
                                          departure_time=datetime.now())
     
-    # Extract travel time from directions_result
     if directions_result:
-        travel_time = directions_result[0]['legs'][0]['duration']['text']
-        return travel_time
+        leg = directions_result[0]['legs'][0]
+        travel_time = leg['duration']['value']  # seconds
+        distance = leg['distance']['value']  # meters
+        return (distance, travel_time)
     else:
-        return None
+        return (0, 0)
+
+def read_locations_from_csv(file_path):
+    locations = []
+    locations.append(start_coords)
+    with open(file_path, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            locations.append((float(row['Lat']), float(row['Lon'])))
+    return locations
+
+def create_distance_matrix(locations):
+    n = len(locations)
+    distance_matrix = [[(0, 0) for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                distance_matrix[i][j] = get_travel_time_and_distance(locations[i], locations[j])
+    return distance_matrix
 
 if __name__ == "__main__":
-    # Input starting location
-    start_lat = float(input("Enter the starting latitude: "))
-    start_lng = float(input("Enter the starting longitude: "))
-    start_coords = (start_lat, start_lng)
+    file_path = 'locations.csv'
+    locations = read_locations_from_csv(file_path)
+    distance_matrix = create_distance_matrix(locations)
     
-    # Input destination location
-    dest_lat = float(input("Enter the destination latitude: "))
-    dest_lng = float(input("Enter the destination longitude: "))
-    dest_coords = (dest_lat, dest_lng)
-    
-    travel_time = get_travel_time(start_coords, dest_coords)
-    
-    if travel_time:
-        print(f"The travel time from {start_coords} to {dest_coords} is {travel_time}.")
-    else:
-        print("Could not find travel time for the given locations.")
+    for row in distance_matrix:
+        print(row)
